3463e6a468b989e37a11c232cb22ca900ae8b307
Christoffer Olsen <chris70944@gmail.com>
2024-03-13 23:25:56 +0100

    Done

--- Ovelse 3.9/Program.cs
+++ Ovelse 3.9/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hvor høj er du (i meter)?");
+
+duuble hojde = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Du er {hojde} meter høj");
